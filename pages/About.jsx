9c9f57de39d1cc356aeafe452d9fe95ef7e8599f
Astha <astha9526@gmail.com>
2025-08-21 00:20:33 +0530

    updatebyASsss

--- pages/About.jsx
+++ pages/About.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='flex justify-center items-center mt-5 ' >
+    <div className='flex justify-center items-center mt-5 flex-col' >
     <div className='text-3xl '  >This is an About page I love you astha</div>
+    <div className='text-2xl '>This my new way of learning </div>
     </div>
   )
 }
