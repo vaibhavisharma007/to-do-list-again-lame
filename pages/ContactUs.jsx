7c6abe5e66c84f1fe6bcd3c5e1c6bd6971b08cb5
vaibhavi <vaibhavisharma473@gmail.com>
2025-08-21 00:12:16 +0530

    updated contact page

--- pages/ContactUs.jsx
+++ pages/ContactUs.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
+    <div className='text-3xl font-bold border-2 border-amber-500' >this is Contact Page</div>
   )
 }
 
