5d64eb0ffb62bd5dfc40b857116f61992e798a13
Nikita Grover <nikiatgrover10864@gmail.com>
2025-08-21 17:07:58 +0530

    hbjhajak

--- pages/ContactUs.jsx
+++ pages/ContactUs.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const ContactUs = () => {
   return (
-    <div className='text-3xl font-bold border-2 border-amber-500' >this is Contact Page</div>
+    <div className='flex text-3xl font-bold border-2 border-amber-500' >this is Contact Page
+    <div onClick= {()=>{alert("helloooo")}} className = 'text-4xl rounded-lg hover:text-2xl cursor-pointer border-4 px-4 m-5 bg-pink-500' > nikitssss</div>
+    </div>
+    
   )
 }
 
